aac5748c9292a7f9a602d168d6340929e3c209e7
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-10-04 06:23:09 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rodrigo Carballo on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
