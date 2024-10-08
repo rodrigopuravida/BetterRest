77c6deb4ddf4f77ea3ba5b56736d3cf493186f87
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-10-07 20:36:26 -0700

    Adding calculations with ML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Rodrigo Carballo on 10/4/24.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showinglert = false
     
     var body: some View {
         
@@ -37,14 +41,38 @@ struct ContentView: View {
             .navigationTitle("Better Rest")
             .toolbar {
                 Button("Calculate", action: calculateBedTimne)
-
+            }
+            .alert(alertTitle, isPresented: $showinglert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
             }
         }
 
     }
     
     func calculateBedTimne() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your Ideal bedtime is..."
+            alertMessage = "\(sleepTime.formatted(date: .omitted, time: .shortened))"
+
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was an error calculating your bedtime."
+        }
         
+        showinglert = true
     }
 }
 
